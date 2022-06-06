e5585cb8b933010b287047f6b033c055f7893de2
jrenzoo <u202118568@upc.edu.pe>
2022-06-06 10:40:36 -0500

    feat: created layers in reviews bc

---
+++ src/reviews/api/reviews.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post } from "@nestjs/common";
+
+
+@Controller('reviews')
+export class ReviewsController{
+
+    constructor(){}
+
+    // @Get()
+    // async getReviews(@Res({ passthrough: true }) response): Promise<object> {
+
+    // }
+
+
+}
