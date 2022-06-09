9274ae19db21f78000be363e429b9b84280824b4
jrenzoo <u202118568@upc.edu.pe>
2022-06-09 09:24:33 -0500

    feat: implemented queries to create tables in common

--- src/common/infrastructure/persistence/typeorm/migrations/1652800321507-InitialSchema.ts
+++ src/common/infrastructure/persistence/typeorm/migrations/1652800321507-InitialSchema.ts
@@ -5,10 +5,12 @@ export class InitialSchema1652800321507 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`customers\` (\`id\` bigint UNSIGNED NOT NULL AUTO_INCREMENT, \`first_name\` varchar(30) NOT NULL, \`last_name\` varchar(30) NOT NULL, \`email\` varchar(150) NOT NULL, \`password\` varchar(15) NOT NULL,\`car_make\` varchar(20) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`mechanics\` (\`id\` bigint UNSIGNED NOT NULL AUTO_INCREMENT, \`name\` varchar(50) NOT NULL, \`email\` varchar(150) NOT NULL, \`password\` varchar(50) NOT NULL, \`address\` varchar(50) NOT NULL,\`description\` varchar(200) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE \`customers\``);
+        await queryRunner.query(`DROP TABLE \`mechanics\``);
     }
 
 }
