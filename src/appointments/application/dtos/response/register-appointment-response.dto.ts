5a24c20d29e49c474ceaa9b1addec04075b697c2
jrenzoo <u202118568@upc.edu.pe>
2022-06-22 10:45:21 -0500

    feat: implemented response dto for register and update

---
+++ src/appointments/application/dtos/response/register-appointment-response.dto.ts
@@ -0,0 +1,11 @@
+export class RegisterAppointmentResponseDto{
+    constructor(
+        public readonly id: number,
+        public readonly customerId: number,
+        public readonly mechanicId: number,
+        public readonly status: string,
+        public readonly type: string,
+        public readonly date: string,
+        public readonly amount: number
+    ){}
+}
