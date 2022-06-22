a0c823c56b48c9c45b73c4e94794d5d1838b3ccd
jrenzoo <u202118568@upc.edu.pe>
2022-06-22 10:17:55 -0500

    feat: Implemented dto for query that gets all data

---
+++ src/appointments/application/dtos/queries/get-appointment.dto.ts
@@ -0,0 +1,9 @@
+export class GetAppointmentsDto {
+    public id: number;
+    public customerId: number;
+    public mechanicId: number;
+    public status: string;
+    public type: string;
+    public date: string;
+    public amount: string;
+}
