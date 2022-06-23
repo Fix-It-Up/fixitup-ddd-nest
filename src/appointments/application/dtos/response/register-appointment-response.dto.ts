b46b4f938b4e7b4f6a3573b45f6944ca1ed5174f
jrenzoo <u202118568@upc.edu.pe>
2022-06-22 19:47:14 -0500

    feat: added services for appointments

--- src/appointments/application/dtos/response/register-appointment-response.dto.ts
+++ src/appointments/application/dtos/response/register-appointment-response.dto.ts
@@ -3,9 +3,7 @@ export class RegisterAppointmentResponseDto{
         public readonly id: number,
         public readonly customerId: number,
         public readonly mechanicId: number,
-        public readonly status: string,
         public readonly type: string,
-        public readonly date: string,
-        public readonly amount: number
+        public readonly date: string
     ){}
 }
