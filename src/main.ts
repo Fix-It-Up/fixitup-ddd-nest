99d91c9869f9488d6432d854af08067e5d540404
jrenzoo <u202118568@upc.edu.pe>
2022-06-29 19:05:59 -0500

    feat: added swagger dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
+  const config = new DocumentBuilder()
+  .setTitle('Fix it Up RESTful API')
+  .setDescription('FiX It Up API documentation')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
 }
 bootstrap();
